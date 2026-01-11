400fa6e8d071748ce5e718d895fdc581246006b7
Paresh Songire <iamps.software@gmail.com>
2026-01-12 00:53:22 +0530

    owner pendrive add 1-12-2026

--- src/main/java/practice/demo/DemoApplication.java
+++ src/main/java/practice/demo/DemoApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class 	DemoApplication {
 
-	//paresh songire
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
