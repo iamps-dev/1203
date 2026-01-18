cad2bbe59dc62cd1a7e919733a6f1fbfabc689a2
Paresh Songire <iamps.software@gmail.com>
2026-01-18 23:35:34 +0530

    admin al

--- src/main/java/practice/demo/DemoApplication.java
+++ src/main/java/practice/demo/DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class 	DemoApplication {
 
-//	paresh songire
+//	paresh songire 1203
 	public static 	void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
